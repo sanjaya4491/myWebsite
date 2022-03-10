b90588c6572aab328c13ecd1da386a575232de82
sanjaya4491 <54679398+sanjaya4491@users.noreply.github.com>
2022-03-09 23:08:27 -0600

    fixes
    fix

--- myFirstProject/Data/AppDbContext.cs
+++ myFirstProject/Data/AppDbContext.cs
@@ -3,21 +3,23 @@ using myFirstProject.Models;
 
 namespace myFirstProject.Data
 {
-    public class AppDbContext:DbContext
+    public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
+
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
             modelBuilder.Entity<Actor_Movie>().HasKey(am => new
             {
                 am.ActorId,
                 am.MovieId
             });
-     
-            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Movie).WithMany(am=> am.Actors_Movies).HasForeignKey(am=>am.MovieId);
+
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Movie).WithMany(am => am.Actors_Movies).HasForeignKey(am => am.MovieId);
 
             modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Actor).WithMany(am => am.Actors_Movies).HasForeignKey(am => am.ActorId);
 
@@ -25,15 +27,15 @@ namespace myFirstProject.Data
             base.OnModelCreating(modelBuilder);
         }
 
-        public DbSet<Actor> Actors{ get; set; }
+        public DbSet<Actor>? Actors { get; set; }
 
-        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Movie>? Movies { get; set; }
 
-        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Producer>? Producers { get; set; }
 
-        public DbSet<Actor_Movie> Actor_Movies { get; set; }
+        public DbSet<Actor_Movie>? Actor_Movies { get; set; }
 
-        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Cinema>? Cinemas { get; set; }
 
     }
 }
